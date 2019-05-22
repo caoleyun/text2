2f7be17432188f5d2d19543f91b950fa0b87269a
caoleyun <244814637@qq.com>
2019-05-22 19:46:14 +0800

    原生项目上传

---
+++ build/precache-manifest.7633da6833fd9115ac1e721b7e811060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4a329b0cdc9cfb9d0dd7d19d513d5c",
+    "url": "/text2/index.html"
+  },
+  {
+    "revision": "ed61ad8a503419d847b9",
+    "url": "/text2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/text2/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "ed61ad8a503419d847b9",
+    "url": "/text2/static/js/main.d1ee74ce.chunk.js"
+  },
+  {
+    "revision": "92faf57e450671b3a40c",
+    "url": "/text2/static/js/runtime~main.abae1fe5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/text2/static/media/logo.5d5d9eef.svg"
+  }
+]);
